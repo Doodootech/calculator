d0c891be27ca1115eab9e2c4561af85394bd9f4e
Doodootech <46285598+Doodootech@users.noreply.github.com>
2020-05-01 04:24:54 +0100

    Update main.js

--- main.js
+++ main.js
@@ -121,3 +121,15 @@ deleteButton.addEventListener('click', button =>{
     calculator.delete()
     calculator.updateDisplay()
 })
+
+async function registerSW() { 
+  if ('serviceWorker' in navigator) { 
+    try {
+      await navigator.serviceWorker.register('./sw.js'); 
+    } catch (e) {
+      alert('ServiceWorker registration failed. Sorry about that.'); 
+    }
+  } else {
+    document.querySelector('.alert').removeAttribute('hidden'); 
+  }
+}
